a34320879190ba8d3fe2b786996d4ae7c5f1f484
lrs-ta <taslima.akter@iem.fraunhofer.de>
2021-01-15 14:36:33 +0100

    created verification task

--- src/main/groovy/de/fraunhofer/iem/plugins/gradleCogniCryptPlugin/CogniCryptPlugin.groovy
+++ src/main/groovy/de/fraunhofer/iem/plugins/gradleCogniCryptPlugin/CogniCryptPlugin.groovy
@@ -1,42 +1,12 @@
 package de.fraunhofer.iem.plugins.gradleCogniCryptPlugin
 
-import boomerang.callgraph.BoomerangICFG
-import boomerang.callgraph.ObservableDynamicICFG
-import boomerang.callgraph.ObservableICFG
-import boomerang.callgraph.ObservableStaticICFG
-import boomerang.preanalysis.BoomerangPretransformer
-import crypto.analysis.CrySLResultsReporter
-import crypto.analysis.CryptoScanner
-import crypto.exceptions.CryptoAnalysisException
-import crypto.reporting.CommandLineReporter
-import crypto.reporting.ErrorMarkerListener
-import crypto.reporting.SARIFReporter
-import crypto.reporting.SourceCodeLocater
-import crypto.rules.CrySLRule
-import crypto.rules.CrySLRuleReader
-import org.gradle.api.GradleException
-
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.component.Artifact
 import org.gradle.api.plugins.quality.CodeQualityExtension
 import org.gradle.api.plugins.quality.internal.AbstractCodeQualityPlugin
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputDirectory
 
-import soot.SceneTransformer
-import soot.SootMethod
-import soot.Transformer
-import soot.Unit
-
-import java.util.regex.Pattern
 
 class CogniCryptPlugin extends AbstractCodeQualityPlugin<SootGradle> {
-
-    private String reportsFolderParameter;
-    private String outputFormat;
-    private boolean dynamicCg;
-    public String rulesDirectory;
-    private Optional<File> targetDir = Optional.empty();
 
     @Override
     protected String getToolName() {
@@ -51,11 +21,6 @@ class CogniCryptPlugin extends AbstractCodeQualityPlugin<SootGradle> {
     protected CodeQualityExtension createExtension() {
         extension=project.getExtensions().create("cognicryptExtension", CogniCryptPluginExtension.class,project);
         extension.setToolVersion(project.properties.get('version'));
-        //extension.setRulesets();
-        //extension.setRuleSetFiles(project.getLayout().files());
-        //conventionMappingOf(extension).map("targetJdk", ()->getDefaultTargetJdk(getJavaPluginConvention().getSourceCompatibility()));
-        validateParameters();
-        createAnalysisTransformer();
         return extension;
     }
 
@@ -80,101 +45,5 @@ class CogniCryptPlugin extends AbstractCodeQualityPlugin<SootGradle> {
     protected void configureTaskDefaults(SootGradle task, String baseName) {
         Configuration configuration = project.getConfigurations().getAt(getConfigurationName());
     }
-
-    @OutputDirectory
-    private File getReportFolder() {
-        File reportsFolder = new File(reportsFolderParameter);
-        if (!reportsFolder.isAbsolute()) {
-            reportsFolder = new File(getProjectTargetDir(), reportsFolderParameter);
-        }
-        if (!reportsFolder.exists()) {
-            reportsFolder.mkdirs();
-        }
-        return reportsFolder;
-    }
-    /**
-     * Receives the set of rules form a given directory.
-     */
-    @InputFiles
-    private List<CrySLRule> getRules() throws CryptoAnalysisException {
-        return CrySLRuleReader.readFromDirectory(new File(rulesDirectory));
-    }
-
-    private void validateParameters() throws GradleException{
-        if (!new File(rulesDirectory).exists() || !new File(rulesDirectory).isDirectory()) {
-            throw new GradleException("Failed to locate the folder of the CrySL rules. " +
-                    "Specify -Dcognicrypt.rulesDirectory=<PATH-TO-CRYSL-RULES>.")
-        }
-        if (!Pattern.matches("(standard|sarif)", outputFormat)) {
-            throw new GradleException("Incorrect output format specified. " +
-                    "Use -Dcognicrypt.outputFormat=[standard|sarif].")
-        }
-    }
-
-    protected Transformer createAnalysisTransformer() {
-        return new SceneTransformer() {
-            @Override
-            protected void internalTransform(String phaseName, Map<String, String> options) {
-                BoomerangPretransformer.v().reset()
-                BoomerangPretransformer.v().apply()
-                final CrySLResultsReporter reporter = new CrySLResultsReporter()
-                ErrorMarkerListener fileReporter
-
-                System.out.println("Fetching CogniCrypt Rules.")
-                List<CrySLRule> rules
-                try {
-                    rules = getRules()
-                } catch (Exception e) {
-                    System.out.println("Failed fetching rules: " + e.getMessage())
-                    return
-                }
-
-                if (outputFormat.equalsIgnoreCase("standard")) {
-                    fileReporter = new CommandLineReporter(getReportFolder().getAbsolutePath(), rules)
-                } else if (outputFormat.equalsIgnoreCase("sarif")) {
-                    fileReporter = new SARIFReporter(getReportFolder().getAbsolutePath(), rules,
-                            new SourceCodeLocater(project.hasParent() ?
-                                    project.getParent() : project.getBasedir()))
-                }
-                else {
-                    throw new RuntimeException("Illegal state")
-                }
-                reporter.addReportListener(fileReporter)
-
-                System.out.println("Creating ICFG!")
-                ObservableICFG<Unit, SootMethod> icfg
-                if (!dynamicCg) {
-                    icfg = new ObservableStaticICFG(new BoomerangICFG(true))
-                } else {
-                    icfg = new ObservableDynamicICFG(false)
-                }
-                CryptoScanner scanner = new CryptoScanner() {
-
-                    @Override
-                    public ObservableICFG<Unit, SootMethod> icfg() {
-                        return icfg
-                    }
-
-                    @Override
-                    public CrySLResultsReporter getAnalysisListener() {
-                        return reporter
-                    }
-                }
-
-                System.out.println("Starting CogniCrypt Analysis!")
-                scanner.scan(rules)
-            }
-        }
-    }
-
-    public File getProjectTargetDir() {
-        if (!targetDir.isPresent()){
-            File td = new File(project.getBuild().getDirectory());
-            targetDir = Optional.of(td);
-        }
-        return targetDir.get();
-    }
-
-
 
 }
--- src/main/groovy/de/fraunhofer/iem/plugins/gradleCogniCryptPlugin/SootGradle.java
+++ src/main/groovy/de/fraunhofer/iem/plugins/gradleCogniCryptPlugin/SootGradle.java
@@ -1,94 +1,22 @@
 package de.fraunhofer.iem.plugins.gradleCogniCryptPlugin;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
+
 import org.gradle.api.Project;
-import org.gradle.api.reporting.Reporting;
-
 import org.gradle.api.tasks.*;
-import soot.PackManager;
-import soot.Transform;
-import soot.Transformer;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @CacheableTask
 public abstract class SootGradle extends SourceTask implements VerificationTask{
 
-    private List<String> classFolders = Lists.newLinkedList();
-    private Optional<File> targetDir = Optional.empty();
-    protected String localRepoProperty;
-    private String callGraph;
-    private boolean includeDependencies;
-    private String excludedPackages;
-
 
     @TaskAction
     public void doExecute(Project project){
-
-            if (!targetDir.isPresent()){
-                File td = project.getBuildFile();
-                targetDir = Optional.of(td);
-            }
-
-
-        run(targetDir.get(),project);
-
-
+        run(project);
     }
 
-    public void run(File targetDir, Project project) {
-        final File classFolder = new File(targetDir.getAbsolutePath() + File.separator + "classes");
-        if(!classFolder.exists()) {
-            project.getLogger().info("No class folder found at " + classFolder + "");
-            return;
-        }
+    public void run(Project project) {
 
-        new SootSetup(CreateSootSetupData()).run();
-        analyse();
-        project.getLogger().info("Soot analysis done!");
+        project.getLogger().info("analysis plugin working!");
     }
 
-    protected abstract Transformer createAnalysisTransformer();
-
-    private SootSetupData CreateSootSetupData() {
-        List<String> excludeList = getExcludeList();
-        List<String> appCp = buildApplicationClassPath();
-        String sootCp = buildSootClassPath(classFolders, appCp);
-        boolean modular = JavaUtils.isModularProject(new File(targetDir.get(), "classes"));
-        return new SootSetupData(callGraph, sootCp, appCp, modular, excludeList);
-    }
-
-    private void analyse() {
-        PackManager.v().getPack("wjap").add(new Transform("wjap.ifds", createAnalysisTransformer()));
-        PackManager.v().runPacks();
-    }
-
-
-
-    private List<String> getExcludeList() {
-        return  Lists.newArrayList(excludedPackages.split(","));
-    }
-
-    @Classpath
-    private List<String> buildApplicationClassPath() {
-        final File classFolder = new File(targetDir.get().getAbsolutePath(), "classes");
-        return Lists.newArrayList(classFolder.getAbsolutePath());
-    }
-
-    @Classpath
-    private String buildSootClassPath(List<String> dependencies, List<String> applicationCp) {
-        List<String> sootCp = Stream.of(dependencies, applicationCp)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-        String javaPath = JavaUtils.getJavaRuntimePath().getAbsolutePath();
-        sootCp.add(0, javaPath);
-        List<String> distinctSootCp = sootCp.stream().distinct().collect(Collectors.toList());
-        return Joiner.on(File.pathSeparator).join(distinctSootCp);
-    }
 }
